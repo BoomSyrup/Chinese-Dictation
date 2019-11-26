ba813437a715459cf60bbe4a707d4bdce26587a3
BoomSyrup <byron4white@gmail.com>
2019-11-25 23:01:00 -0800

    Rechange the way the pinyin table is shown

---
+++ src/Table1.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './PinYin.css';
+
+class Table1 extends React.Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th></th><th>a</th><th>o</th><th>e</th><th>-i</th><th>er</th><th>ai</th><th>ei</th><th>ao</th><th>ou</th><th>an</th><th>en</th><th>ang</th><th>eng</th><th>ong</th><th>i</th><th>ia</th><th>iao</th><th>ie</th><th>iu</th><th>ian</th><th>in</th><th>iang</th><th>ing</th><th>iong</th><th>u</th><th>ua</th><th>uo</th><th>uai</th><th>ui</th><th>uan</th><th>un</th><th>uang</th><th>ueng</th><th>ü</th><th>üe</th><th>üan</th><th>ün</th>
+                    </tr> 
+                    <tr>
+                        <th></th><th>a</th><th>o</th><th>e</th><th>-i</th><th>er</th><th>ai</th><th>ei</th><th>ao</th><th>ou</th><th>an</th><th>en</th><th>ang</th><th>eng</th><th>ong</th><th>i</th><th>ia</th><th>iao</th><th>ie</th><th>iu</th><th>ian</th><th>in</th><th>iang</th><th>ing</th><th>iong</th><th>u</th><th>ua</th><th>uo</th><th>uai</th><th>ui</th><th>uan</th><th>un</th><th>uang</th><th>ueng</th><th>ü</th><th>üe</th><th>üan</th><th>ün</th>
+                    </tr> 
+                    <tr>
+                        <th></th><th>a</th><th>o</th><th>e</th><th>-i</th><th>er</th><th>ai</th><th>ei</th><th>ao</th><th>ou</th><th>an</th><th>en</th><th>ang</th><th>eng</th><th>ong</th><th>i</th><th>ia</th><th>iao</th><th>ie</th><th>iu</th><th>ian</th><th>in</th><th>iang</th><th>ing</th><th>iong</th><th>u</th><th>ua</th><th>uo</th><th>uai</th><th>ui</th><th>uan</th><th>un</th><th>uang</th><th>ueng</th><th>ü</th><th>üe</th><th>üan</th><th>ün</th>
+                    </tr> 
+                    <tr>
+                        <th></th><th>a</th><th>o</th><th>e</th><th>-i</th><th>er</th><th>ai</th><th>ei</th><th>ao</th><th>ou</th><th>an</th><th>en</th><th>ang</th><th>eng</th><th>ong</th><th>i</th><th>ia</th><th>iao</th><th>ie</th><th>iu</th><th>ian</th><th>in</th><th>iang</th><th>ing</th><th>iong</th><th>u</th><th>ua</th><th>uo</th><th>uai</th><th>ui</th><th>uan</th><th>un</th><th>uang</th><th>ueng</th><th>ü</th><th>üe</th><th>üan</th><th>ün</th>
+                    </tr> 
+                </thead>
+            </table>
+        );
+    }
+}
+export default Table1
---
+++ src/Table2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './PinYin.css';
+
+class Table2 extends React.Component {
+
+}
+export default Table2
